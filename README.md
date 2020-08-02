7690d0fe890fda64f1f7031807c272c0522a8ffe
Artem Bondarenko <artem.arturovich.bondarenko@gmail.com>
2020-08-02 09:23:13 +0300

    hw-08

---
+++ README.md
@@ -0,0 +1,16 @@
+## Phonebook
+
+https://anton-ua.github.io/goit-react-hw-08-phonebook
+
+
+
+----
+### Technology Stack
+- React
+- Redux
+- React-Bootstrap
+- Formik
+- Axios
+- Redux Persist
+  
+[Backend](https://goit-phonebook-api.herokuapp.com/docs/)
---
+++ build/precache-manifest.c97807c75ceecb973f3801e3981c23ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53bf3f7b86244a6c134cdd870fd27eeb",
+    "url": "/goit-react-hw-08-phonebook/index.html"
+  },
+  {
+    "revision": "8fb40b6d6640ded5b3fe",
+    "url": "/goit-react-hw-08-phonebook/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "694df3c3c548577557a2",
+    "url": "/goit-react-hw-08-phonebook/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "8fb40b6d6640ded5b3fe",
+    "url": "/goit-react-hw-08-phonebook/static/js/2.e4ae51e0.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/goit-react-hw-08-phonebook/static/js/2.e4ae51e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "694df3c3c548577557a2",
+    "url": "/goit-react-hw-08-phonebook/static/js/main.a5cd09d1.chunk.js"
+  },
+  {
+    "revision": "bd4c05e82bd184cf56fd",
+    "url": "/goit-react-hw-08-phonebook/static/js/runtime-main.8d8c96bc.js"
+  }
+]);
